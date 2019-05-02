4d295f3ea29b7c98e20624018e58892d5dc7fca8
simo <med.elkhalki0@gmail.com>
2019-05-02 17:17:58 +0100

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,88 +1,89 @@
 export default {
-  apiUrl: 'http://joeylin.net/api/',
+    apiUrl: 'http://joeylin.net/api/',
 };
 
 const siteConfig = {
-  siteName: 'Isomorphic',
-  siteIcon: 'ion-beer',
-  footerText: 'Isomorphic ©2019 Created with LOVE by Med',
+    siteName: 'Isomorphic',
+    siteIcon: 'ion-beer',
+    footerText: 'Isomorphic ©2019 Created with LOVE by Med',
 };
 
 
 const themeConfig = {
-  topbar: 'themedefault',
-  sidebar: 'themedefault',
-  layout: 'themedefault',
-  theme: 'themedefault',
+    topbar: 'themedefault',
+    sidebar: 'themedefault',
+    layout: 'themedefault',
+    theme: 'themedefault',
 };
 
 const language = 'english';
+const language = 'english';
 
 const AlgoliaSearchConfig = {
-  appId: '',
-  apiKey: '',
+    appId: '',
+    apiKey: '',
 };
 
 const Auth0Config = {
-  domain: '',
-  clientID: '', //
-  options: {
-    auth: {
-      autoParseHash: true,
-      redirect: false,
-    },
-    languageDictionary: {
-      title: 'joeylin.net',
-      emailInputPlaceholder: 'demo@gmail.com',
-      passwordInputPlaceholder: 'demodemo',
-    },
-    icon: '',
-    theme: {
-      labeledSubmitButton: true,
-      logo: 'https://s3.amazonaws.com/redqteam.com/logo/isomorphic.png',
-      primaryColor: '#E14615',
-      authButtons: {
-        connectionName: {
-          displayName: 'Log In',
-          primaryColor: '#b7b7b7',
-          foregroundColor: '#000000',
-          icon: undefined,
+    domain: '',
+    clientID: '', //
+    options: {
+        auth: {
+            autoParseHash: true,
+            redirect: false,
         },
-      },
+        languageDictionary: {
+            title: 'joeylin.net',
+            emailInputPlaceholder: 'demo@gmail.com',
+            passwordInputPlaceholder: 'demodemo',
+        },
+        icon: '',
+        theme: {
+            labeledSubmitButton: true,
+            logo: 'https://s3.amazonaws.com/redqteam.com/logo/isomorphic.png',
+            primaryColor: '#E14615',
+            authButtons: {
+                connectionName: {
+                    displayName: 'Log In',
+                    primaryColor: '#b7b7b7',
+                    foregroundColor: '#000000',
+                    icon: undefined,
+                },
+            },
+        },
     },
-  },
 };
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
 };
 
 const googleConfig = {
-  apiKey: '', //
+    apiKey: '', //
 };
 
 const mapboxConfig = {
-  tileLayer: '',
-  maxZoom: '',
-  defaultZoom: '',
-  center: [],
+    tileLayer: '',
+    maxZoom: '',
+    defaultZoom: '',
+    center: [],
 };
 
 const youtubeSearchApi = 'AIzaSyD62G9sk4Mk66uj28FeWoreQLLlWcy7k3o';
 
 export {
-  siteConfig,
-  themeConfig,
-  language,
-  AlgoliaSearchConfig,
-  Auth0Config,
-  firebaseConfig,
-  googleConfig,
-  mapboxConfig,
-  youtubeSearchApi,
+    siteConfig,
+    themeConfig,
+    language,
+    AlgoliaSearchConfig,
+    Auth0Config,
+    firebaseConfig,
+    googleConfig,
+    mapboxConfig,
+    youtubeSearchApi,
 };
