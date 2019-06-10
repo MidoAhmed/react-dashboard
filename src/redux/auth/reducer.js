80edbb832eb8e871493ff8d1547d20aad6a92243
simo <med.elkhalki0@gmail.com>
2019-06-10 14:32:45 +0100

    implement signIn

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -1,21 +1,22 @@
-import { Map } from 'immutable';
-import { getToken } from '../../helpers/utility';
+import {Map} from 'immutable';
+import {getToken} from '../../helpers/utility';
 import actions from './actions';
 
 const initState = new Map({
-  idToken: 'secret token'
+    idToken: null,
+    loading: false
 });
 
-export default function authReducer(
-  state = initState.merge(getToken()),
-  action
-) {
-  switch (action.type) {
-    case actions.LOGIN_SUCCESS:
-      return state.set('idToken', action.token);
-    case actions.LOGOUT:
-      return initState;
-    default:
-      return state;
-  }
+export default function authReducer(state = initState.merge(getToken()), action) {
+    switch (action.type) {
+        case actions.LOGIN_REQUEST:
+            console.log('loading', action);
+            return state.set('loading', true);
+        case actions.LOGIN_SUCCESS:
+            return state.set('idToken', action.token).set('loading', false);
+        case actions.LOGOUT:
+            return initState;
+        default:
+            return state;
+    }
 }
--- src/redux/auth/saga.js
+++ src/redux/auth/saga.js
@@ -1,45 +1,61 @@
-import { all, takeEvery, put, fork } from 'redux-saga/effects';
-import { push } from 'react-router-redux';
-import { clearToken } from '../../helpers/utility';
+import {all, call, takeEvery, put, fork} from 'redux-saga/effects';
+import {push} from 'react-router-redux';
+import {clearToken} from '../../helpers/utility';
 import actions from './actions';
+import auth from '../../services/auth.service'
 
-const fakeApiCall = true; // auth0 or express JWT
+const fakeApiCall = false; // auth0 or express JWT
 
 export function* loginRequest() {
-  yield takeEvery('LOGIN_REQUEST', function*() {
-    if (fakeApiCall) {
-      yield put({
-        type: actions.LOGIN_SUCCESS,
-        token: 'secret token',
-        profile: 'Profile'
-      });
-    } else {
-      yield put({ type: actions.LOGIN_ERROR });
-    }
-  });
+    yield takeEvery(actions.LOGIN_REQUEST, function* () {
+        const payload = {
+            email: "lorem",
+            password: "password"
+        };
+        const res = yield call(auth.signIn, payload)
+        const body = yield res.json();
+        yield put({
+            type: actions.LOGIN_SUCCESS,
+            token: body.user.token,
+            profile: body.user
+        });
+
+        /*if (fakeApiCall) {
+          yield put({
+            type: actions.LOGIN_SUCCESS,
+            token: 'secret token',
+            // profile: 'Profile'
+          });
+        } else {
+          yield put({ type: actions.LOGIN_ERROR });
+        }*/
+    });
 }
 
 export function* loginSuccess() {
-  yield takeEvery(actions.LOGIN_SUCCESS, function*(payload) {
-    yield localStorage.setItem('id_token', payload.token);
-  });
+    yield takeEvery(actions.LOGIN_SUCCESS, function* (payload) {
+        yield localStorage.setItem('id_token', payload.token);
+        yield put(push('/dashboard'));
+    });
 }
 
 export function* loginError() {
-  yield takeEvery(actions.LOGIN_ERROR, function*() {});
+    yield takeEvery(actions.LOGIN_ERROR, function* () {
+    });
 }
 
 export function* logout() {
-  yield takeEvery(actions.LOGOUT, function*() {
-    clearToken();
-    yield put(push('/'));
-  });
+    yield takeEvery(actions.LOGOUT, function* () {
+        clearToken();
+        yield put(push('/'));
+    });
 }
+
 export default function* rootSaga() {
-  yield all([
-    fork(loginRequest),
-    fork(loginSuccess),
-    fork(loginError),
-    fork(logout)
-  ]);
+    yield all([
+        fork(loginRequest),
+        fork(loginSuccess),
+        fork(loginError),
+        fork(logout)
+    ]);
 }
